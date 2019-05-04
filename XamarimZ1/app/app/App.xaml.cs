e504674b0cba1ce5d44b633519bd1cf535cb542c
Samuel Peña <samuel_pena@hotmail.es>
2019-05-04 11:12:19 -0400

    Ultimo commit de 4 de mayo

--- XamarimZ1/app/app/App.xaml.cs
+++ XamarimZ1/app/app/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using app.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace app
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Page1(); //en de loginpage puse page1
         }
 
         protected override void OnStart()
