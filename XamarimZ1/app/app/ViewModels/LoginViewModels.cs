a51f221d82d0b3f65be509f3d06f6bcbe7e23b53
Samuel Peña <samuel_pena@hotmail.es>
2019-05-11 11:19:51 -0400

    con 2 errores

---
+++ XamarimZ1/app/app/ViewModels/LoginViewModels.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace app.ViewModels
+{
+    class LoginViewModels : BaseViewModel
+    {
+        #region variables
+        string email;
+        string password;
+        bool isrunning;
+        #endregion
+
+        #region propiedades
+        public string Email
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+        public string Password {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+        public bool IsRunning {
+            get { return this.isrunning; }
+            set { SetValue(ref this.isrunning, value); }
+
+            #endregion
+            #region comandos
+            public ICommand LoginCommand {
+            get { return new RelayCommand(Login);
+            }
+            #endregion
+        }
+
+        private void Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XamarimZ1/app/app/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app.ViewModels
+{
+        //using Models;
+   using System;
+   using System.Collections.ObjectModel;
+
+   public class MainViewModel
+    {
+        #region Properties
+        public string Token { get; set; }
+        public string TokenType { get; set; }
+        #endregion
+
+        #region ViewModels
+        public LoginViewModels Login { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new LoginViewModels(); //secambio aca se suma una s
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+            }
+            return instance;
+        }
+        #endregion
+
+    }
+}
+
